e61ee0f89b61178f1b9f5dc35c74732cda7ae1c0
brandadr <brandadrian@gmail.com>
2022-01-14 22:52:17 +0100

    Initial

---
+++ DeadlockSimulatorWpf/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DeadlockSimulatorWpf
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"Button_Click start; ThreadId; {Thread.CurrentThread.ManagedThreadId}");
+            var task = ExecuteAsync();
+            Debug.WriteLine($"Set current thread on wait; ThreadId; {Thread.CurrentThread.ManagedThreadId}");
+            task.Wait();
+            Debug.WriteLine($"Button_Click done; ThreadId; {Thread.CurrentThread.ManagedThreadId}");
+            Button1.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private async Task ExecuteAsync()
+        {
+            Debug.WriteLine($"ExecuteAsync start; ThreadId; {Thread.CurrentThread.ManagedThreadId}");
+            //No Deadlock
+            await Task.Delay(2000).ConfigureAwait(false); // Will continue the following code in different thread than caller.
+            //Deadlock
+            //await Task.Delay(2000);
+            Debug.WriteLine($"ExecuteAsync done; ThreadId; {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
